e44867e0378dd8c635d3b7c1d07413129cd08f73
Felipe Parreira <felipe@suthub.com>
2021-03-20 15:49:04 -0300

    added user creation and JWT authentication

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const restify = require('restify');
 const mongoose = require('mongoose');
 const config = require('./config');
+const rjwt = require('restify-jwt-community');
 
 const server = restify.createServer();
 
 //Middleware
 server.use(restify.plugins.bodyParser());
+
+//Protect Routes
+server.use(rjwt({ secret: config.JWT_SECRET }).unless({path: ['/auth']}));
 
 server.listen(config.PORT, () => {
     mongoose.connect(
@@ -21,5 +25,6 @@ db.on('error', (err) => console.log(err));
 db.once('open', () => {
     console.log('connected')
     require('./routes/beneficiaries')(server)
+    require('./routes/users')(server)
     console.log(`Server started on port ${config.PORT}`)
 });
